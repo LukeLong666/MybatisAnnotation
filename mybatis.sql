c2accff54cc9846a0e8172a43edccd52630facbc
lukelong <lukelongdad@gmail.com>
2020-06-01 23:00:42 +0800

    add sql

---
+++ mybatis.sql
@@ -0,0 +1,75 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.2 
+MySQL - 5.0.22-community-nt : Database - mybatis
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mybatis` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `mybatis`;
+
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `ID` int(11) NOT NULL COMMENT '编号',
+  `UID` int(11) default NULL COMMENT '用户编号',
+  `MONEY` double default NULL COMMENT '金额',
+  PRIMARY KEY  (`ID`),
+  KEY `FK_Reference_8` (`UID`),
+  CONSTRAINT `FK_Reference_8` FOREIGN KEY (`UID`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `account` */
+
+insert  into `account`(`ID`,`UID`,`MONEY`) values (1,46,1000),(2,45,1000),(3,46,2000);
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL COMMENT '下单用户id',
+  `number` varchar(32) NOT NULL COMMENT '订单号',
+  `createtime` datetime default NULL COMMENT '创建订单时间',
+  `note` varchar(100) default NULL COMMENT '备注',
+  PRIMARY KEY  (`id`),
+  KEY `FK_orders_1` (`user_id`),
+  CONSTRAINT `FK_orders_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `orders` */
+
+insert  into `orders`(`id`,`user_id`,`number`,`createtime`,`note`) values (3,41,'1000010','2015-02-04 13:22:35',NULL),(4,41,'1000011','2015-02-03 13:22:41',NULL),(5,41,'1000012','2015-02-12 16:13:23',NULL),(6,41,'123',NULL,'123'),(7,41,'123',NULL,'123');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(32) NOT NULL COMMENT '用户名称',
+  `birthday` datetime default NULL COMMENT '生日',
+  `sex` char(1) default NULL COMMENT '性别',
+  `address` varchar(256) default NULL COMMENT '地址',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`birthday`,`sex`,`address`) values (41,'老王','2018-02-27 17:47:08','男','北京'),(42,'小二王','2018-03-02 15:09:37','女','北京金燕龙'),(43,'小二王','2018-03-04 11:34:34','女','北京金燕龙'),(45,'传智播客','2018-03-04 12:04:06','男','北京金燕龙'),(46,'张','2017-10-10 00:00:00','男','河南省郑州市'),(50,'mybatis saveuser user','2020-04-15 21:19:40','男','河南省郑州苏hi'),(51,'mybatis saveuser last insert id','2020-04-28 11:18:28','男','河南省郑州苏hi'),(52,'mybatis saveuser modify propery','2020-04-28 11:37:02','男','河南省郑州苏hi'),(53,'modify User property','2020-04-29 10:52:11','男','北京市顺义区'),(55,'mybatis annotation',NULL,'女','河南省郑州市'),(56,'张','2017-10-10 00:00:00','男','河南省郑州市');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
